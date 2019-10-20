96b9849acee6458ec5a4356fb1649b498302580b
yushanghh <2534618823@qq.com>
2019-10-20 22:12:52 +0800

    Update RunMain.java

--- mavenDemo/src/main/java/classifier/RunMain.java
+++ mavenDemo/src/main/java/classifier/RunMain.java
@@ -25,57 +25,11 @@ public class RunMain {
 	public static void main(String[] args) throws Exception {
 
 		String savedModelTarPath = System.getenv(ConfigConstant.IMAGE_MODEL_PACKAGE_PATH);
-//		String savedModelTarPath = "/home/cm/workspace/classifier/model.tar.gz";
 		boolean ifReverseInputChannels = true;
 		int[] inputShape = {1, 224, 224, 3};
 		float[] meanValues = {15.68f, 8.78f, 5.94f};
 		float scale = 1.0f;
 		String input = "input_1";
-
-//		String imagePath = "/home/cm/workspace/classifier/figures/baozhi/";
-//		List<LocatedFileStatus> currentFileList;
-//		currentFileList = new ArrayList();
-//		Path imageRoot = new Path(imagePath);
-//		org.apache.hadoop.conf.Configuration hadoopConfig = new org.apache.hadoop.conf.Configuration();
-//		hadoopConfig.set("fs.hdfs.impl", DistributedFileSystem.class.getName());
-//		hadoopConfig.set("fs.file.impl", LocalFileSystem.class.getName());
-//		FileSystem fileSystem = FileSystem.get(new URI(imagePath), hadoopConfig);
-//		RemoteIterator<LocatedFileStatus> it = fileSystem.listFiles(imageRoot, true);
-//		int i = 0;
-//		while(it.hasNext()) {
-//			LocatedFileStatus locatedFileStatus = (LocatedFileStatus)it.next();
-//			if (!locatedFileStatus.getPath().getName().startsWith(".")) {
-//				currentFileList.add(locatedFileStatus);
-//				++i;
-//			}
-//		}
-//
-//		Iterator var2 = currentFileList.iterator();
-//		while(var2.hasNext()) {
-//			LocatedFileStatus fileStatus = (LocatedFileStatus)var2.next();
-//			ImageData imageLabel = new ImageData();
-//			imageLabel.setId(fileStatus.getPath().getName());
-//			long fileLength = fileStatus.getLen();
-//			FSDataInputStream in = fileSystem.open(fileStatus.getPath());
-//			byte[] buffer = new byte[(int)fileLength];
-//			in.readFully(buffer);
-//			in.close();
-//			imageLabel.setImage(buffer);
-//			Image originImage = Toolkit.getDefaultToolkit().createImage(imageLabel.getImage());
-//			ImagePlus imagePlus = new ImagePlus(imageLabel.getId(), originImage);
-//			ImageProcessor imageProcessor = imagePlus.getProcessor();
-//			ImageProcessor resizeProcessor =  imageProcessor.resize(224, 224);
-//			imagePlus.setProcessor(resizeProcessor);
-//			BufferedImage newBuffered = imagePlus.getBufferedImage();
-//			ImageIO.write(newBuffered, "JPG", new File("/home/cm/Desktop/" + String.valueOf((Math.random() * 10)) + ".jpg"));
-//		}
-
-
-
-
-
-
-
 
 		long fileSize = new File(savedModelTarPath).length();
 		InputStream inputStream = new FileInputStream(savedModelTarPath);
@@ -95,11 +49,6 @@ public class RunMain {
 						meanValues, scale, input)).setParallelism(1)
 				.addSink(new ImageClassSink()).setParallelism(1);
 		flinkEnv.execute();
-
-//		flinkEnv.addSource(source).setParallelism(1)
-//				.flatMap(new DebugFlatMap()).setParallelism(4)
-//				.addSink(new ImageClassSink()).setParallelism(1);
-//		flinkEnv.execute();
 
 	}
 
